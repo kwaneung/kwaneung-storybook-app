f4d08ee328ac1bff80776d93ece2e14c91fc89ca
김관응 <whatsup95@naver.com>
2025-07-10 23:03:53 +0900

    Feat: 스타일 내보내기 추가

--- index.ts
+++ index.ts
@@ -13,3 +13,13 @@ export { cn } from "./lib/utils";
 
 // Types
 export type { VariantProps } from "class-variance-authority";
+
+/*
+ * CSS 사용 방법:
+ *
+ * 1. 프로젝트 루트 CSS 파일에 아래 코드 추가:
+ * @import "@kwaneung/design-system/styles";
+ *
+ * 2. 또는 JavaScript/TypeScript 파일에서:
+ * import "@kwaneung/design-system/styles";
+ */
